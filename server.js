c529f690ec9e20ba68334382ff08bd7a80dda5f5
skar <mohamedaliop550@gmail.com>
2026-04-22 18:52:28 +0200

    full site update

--- server.js
+++ server.js
@@ -1,15 +1,17 @@
 const express = require("express");
 const path = require("path");
+
 const app = express();
 
-// يخلي كل الملفات تشتغل (html, css, js)
-app.use(express.static(path.join(__dirname)));
+// يخدم كل الملفات (HTML, CSS, JS)
+app.use(express.static(__dirname));
 
-// الصفحة الأولى (الرئيسية)
+// الصفحة الرئيسية (home.html)
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "home.html"));
 });
 
+// تشغيل السيرفر
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log("Server running on port " + PORT);
